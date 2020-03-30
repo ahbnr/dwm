15bb2b9092b62612ec7ea85875784cd4136a2da3
Anton Haubner <anton.haubner@outlook.de>
2020-03-30 20:33:25 +0200

    Add build script, patches and config

---
+++ gruvbox_colors.h
@@ -0,0 +1,44 @@
+// GruvBox Colors
+// From https://github.com/gruvbox-community/gruvbox-contrib
+static const char gruvbox_dark0_hard[]      = "#1D2021";
+static const char gruvbox_dark0[]           = "#282828";
+static const char gruvbox_dark0_soft[]      = "#32302F";
+static const char gruvbox_dark1[]           = "#3c3836";
+static const char gruvbox_dark2[]           = "#504945";
+static const char gruvbox_dark3[]           = "#665C54";
+static const char gruvbox_dark4[]           = "#7C6F64";
+
+static const char gruvbox_gray_245[]        = "#928374";
+static const char gruvbox_gray_244[]        = "#928374";
+
+static const char gruvbox_light0_hard[]     = "#F9F5D7";
+static const char gruvbox_light0[]          = "#FBF1C7";
+static const char gruvbox_light0_soft[]     = "#F2E5BC";
+static const char gruvbox_light1[]          = "#EBDBB2";
+static const char gruvbox_light2[]          = "#D5C4A1";
+static const char gruvbox_light3[]          = "#BDAE93";
+static const char gruvbox_light4[]          = "#A89984";
+
+static const char gruvbox_bright_red[]      = "#FB4934";
+static const char gruvbox_bright_green[]    = "#B8BB26";
+static const char gruvbox_bright_yellow[]   = "#FABD2F";
+static const char gruvbox_bright_blue[]     = "#83A598";
+static const char gruvbox_bright_purple[]   = "#D3869B";
+static const char gruvbox_bright_aqua[]     = "#8EC07C";
+static const char gruvbox_bright_orange[]   = "#FE8019";
+
+static const char gruvbox_neutral_red[]     = "#CC241D";
+static const char gruvbox_neutral_green[]   = "#98971A";
+static const char gruvbox_neutral_yellow[]  = "#D79921";
+static const char gruvbox_neutral_blue[]    = "#458588";
+static const char gruvbox_neutral_purple[]  = "#B16286";
+static const char gruvbox_neutral_aqua[]    = "#689D6A";
+static const char gruvbox_neutral_orange[]  = "#D65D0E";
+
+static const char gruvbox_faded_red[]       = "#9D0006";
+static const char gruvbox_faded_green[]     = "#79740E";
+static const char gruvbox_faded_yellow[]    = "#B57614";
+static const char gruvbox_faded_blue[]      = "#076678";
+static const char gruvbox_faded_purple[]    = "#8F3F71";
+static const char gruvbox_faded_aqua[]      = "#427B58";
+static const char gruvbox_faded_orange[]    = "#AF3A03";
